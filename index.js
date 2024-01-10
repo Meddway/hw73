96a9b9e4223074cd40dac2e2f25f4d94aeb04f37
Oleg Medvedev <meddway@gmail.com>
2024-01-11 05:47:21 +0600

    Выполнена 2я часть задания, по шифрованию и дешифрованияю текста, с последующим его отображением на странице

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const Vigenere = require('caesar-salad').Vigenere;
+const password = 'password'
+
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
 
 app.get('/:message', (req, res) => {
   const message = req.params.message;
   res.send(message);
+});
+
+app.get('/encode/:message', (req, res) => {
+  const message = req.params.message;
+  res.send(Vigenere.Cipher(password).crypt(message));
+});
+
+app.get('/decode/:message', (req, res) => {
+  const message = req.params.message;
+  res.send(Vigenere.Decipher(password).crypt(message))
 });
 
 app.listen(port, () => {
