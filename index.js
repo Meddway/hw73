2e9a2afba7f9a6a9a9467d1147ee4316735ab57f
Oleg Medvedev <meddway@gmail.com>
2024-01-10 12:01:25 +0600

    Выполнена 1я часть задания, по получению ответа от сервера

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.get('/:message', (req, res) => {
+  const message = req.params.message;
+  res.send(message);
+});
+
+app.listen(port, () => {
+  console.log('Server online on ' + port);
+});
